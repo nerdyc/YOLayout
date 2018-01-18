b33ac9e58f03ca4a9722bd5b4805369b20f53ad7
Christian Niles <cniles@twitter.com>
2018-01-18 10:56:48 -0800

    No need to expose YOLayoutableView

--- YOLayout-iOS/YOLayout.h
+++ YOLayout-iOS/YOLayout.h
@@ -8,7 +8,6 @@ FOUNDATION_EXPORT double YOLayout_VersionNumber;
 FOUNDATION_EXPORT const unsigned char YOLayout_VersionString[];
 
 #import <YOLayout/YOLayout+Core.h>
-#import <YOLayout/YOLayoutableView.h>
 #import <YOLayout/YOLayoutOptions.h>
 #import <YOLayout/YOView.h>
 #import <YOLayout/UIView+YOLayout.h>
