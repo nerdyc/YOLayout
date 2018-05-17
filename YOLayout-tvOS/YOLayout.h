6a8b5a3dcbbece6d550f0487cc556ee2e69f15c1
Christian Niles <cniles@twitter.com>
2018-05-16 23:00:27 -0700

    Add tvOS support.

---
+++ YOLayout-tvOS/YOLayout.h
@@ -0,0 +1,15 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for YOLayout.
+FOUNDATION_EXPORT double YOLayout_VersionNumber;
+
+//! Project version string for YOLayout.
+FOUNDATION_EXPORT const unsigned char YOLayout_VersionString[];
+
+#import <YOLayout/YOLayout+Core.h>
+#import <YOLayout/YOLayout+Logic.h>
+#import <YOLayout/YOLayout+Internal.h>
+#import <YOLayout/YOLayoutOptions.h>
+#import <YOLayout/YOView.h>
+#import <YOLayout/UIView+YOLayout.h>
